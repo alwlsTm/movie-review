dcdb2056b380a721a417529d9caf92069ab1a8f6
alwlsTm <mijine0716@naver.com>
2022-05-15 16:52:08 +0900

    20220515 영화 최신순/베스트순 정렬 선택 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import ReviewList from "./ReviewList";
 import items from '../mock.json';
+import { useState } from "react";
 
 function App() {
+  const [order, setOrder] = useState('createdAt'); //아이템 정렬 state
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);  //아이템 정렬(내림차순)
+
+  const handleNewestClick = () => setOrder('createdAt');  //최신순
+
+  const handleBestClick = () => setOrder('rating');  //베스트순
+
   return (
     <div>
-      <ReviewList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleBestClick}>베스트순</button>
+      <ReviewList items={sortedItems} />
     </div>
   );
 }
